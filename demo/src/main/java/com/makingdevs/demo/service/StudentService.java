03789c490237b2c0ea7598865b8d625e9a881738
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-19 15:16:53 -0600

    Creating a new service with StudentDao & FakeStudentDao

---
+++ demo/src/main/java/com/makingdevs/demo/service/StudentService.java
@@ -0,0 +1,37 @@
+package com.makingdevs.demo.service;
+
+import com.makingdevs.demo.dao.StudentDao;
+import com.makingdevs.demo.model.Student;
+import java.util.List;
+import java.util.UUID;
+
+public interface StudentService {
+
+	private final StudentDao studentDao;
+
+	public StudentService(StudentDao studentDao){
+		this.studentDao = studentDao;
+	}
+
+	public int persitNewStudent(UUID studentId, Student student){
+		UUID studentId = studentId == null ? UUID.randomUUID() : studentId;
+		return studentDao.insertNewStudent(studentId, student);
+	}
+
+	public Student getStudentById(UUID studentId){
+		return studentDao.selectStudentById(studentId);
+	}
+
+	public List<Student> getAllStudents(){
+		return studentDao.selectAllStudents();
+	}
+
+	public int updateStudentById(UUID studentId, Student studentUpdate){
+		return studentDao.updateStudentById(studentId, studentUpdate);
+	}
+
+	public int deleteStudentById(UUID studentId){
+		return studentDao.deleteStudentById(studentId);
+	}
+
+}

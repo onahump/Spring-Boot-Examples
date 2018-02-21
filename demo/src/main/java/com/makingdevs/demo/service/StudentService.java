8944604606933edc2533c8673a58cd580496e84b
onahump <oswaldo@makingdevs.com>
2018-02-20 23:58:52 -0600

    Marcando como servicio a la clase

--- demo/src/main/java/com/makingdevs/demo/service/StudentService.java
+++ demo/src/main/java/com/makingdevs/demo/service/StudentService.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Service
-public interface StudentService {
+public class StudentService {
 
 	private final StudentDao studentDao;
 

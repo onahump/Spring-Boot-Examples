5b951febe07b38c20d975d3cc47c0ef8a62cae87
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-21 12:32:23 -0600

    Creating the methos get & post

--- demo/src/main/java/com/makingdevs/demo/resource/StudentResource.java
+++ demo/src/main/java/com/makingdevs/demo/resource/StudentResource.java
@@ -4,11 +4,11 @@ import com.makingdevs.demo.model.Student;
 import com.makingdevs.demo.service.StudentService;
 
 import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/students")
@@ -28,5 +28,23 @@ public class StudentResource{
 
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+
+    public Student getStudendbyId(@PathVariable("studentId") UUID studentId){
+        return studentService.getStudentById(studentId);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void insertNewStudent(@RequestBody Student student){
+        studentService.persistNewStudent(UUID.randomUUID(), student);
     }
 }

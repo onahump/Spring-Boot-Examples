4517612722936ca9a710e66da6a0281c72cb20af
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-22 13:52:24 -0600

    Creating the address test

--- src/main/java/com/makingdevs/demo/model/Address.groovy
+++ src/main/java/com/makingdevs/demo/model/Address.groovy
@@ -1,17 +1,13 @@
 package com.makingdevs.demo.model
 
 public class Address{
-	
+
 	private String street
-	private String outdoorNumber
-	private String interiorNumber 
-	private String postCode
+	private Integer outdoorNumber
+	private Integer interiorNumber
+	private Integer postCode
 	private String neighborhood
 	private String state 
 	private String city
 	private String country
-
-	public String toString(){
-		"""${street} ${outdoorNumber} ${interiorNumber} ${postCode} ${neighborhood} ${state} ${city} ${country} """
-	}
 }

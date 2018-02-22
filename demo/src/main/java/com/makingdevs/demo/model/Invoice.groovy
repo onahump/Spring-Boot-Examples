b12cbeaa9cbf885b7655002eddcdeff66d4f19e6
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-22 11:47:40 -0600

    Deleting innecesary files

--- demo/src/main/java/com/makingdevs/demo/model/Invoice.groovy
+++
@@ -1,8 +0,0 @@
-package com.makingdevs.demo.model
-
-public class Invoice {
-	private final Date date = new Date()
-	private final InvoiceEntity emitter
-	private final InvoiceEntity receiver
-	private final List<Concept> concepts 
-}

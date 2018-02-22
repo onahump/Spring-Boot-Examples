b1c89a01acccaa5276954159b323d1ce717c31ea
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-22 13:53:26 -0600

    Creating the Concept model

--- src/main/java/com/makingdevs/demo/model/Concept.groovy
+++ src/main/java/com/makingdevs/demo/model/Concept.groovy
@@ -4,10 +4,5 @@ public class Concept {
 	private Integer quantity
 	private String product
 	private BigDecimal price
-
-	public String toString(){
-		"Cantidad: ${quantity} Producto: ${product}  Price: ${price}"
-	}
-
 }
 

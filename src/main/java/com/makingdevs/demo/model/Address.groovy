8c28322fc3a4e2e34c715f7449d7e3ea5a951ea8
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 13:21:23 -0600

    Creating the Entity into Address class & generating its id

--- src/main/java/com/makingdevs/demo/model/Address.groovy
+++ src/main/java/com/makingdevs/demo/model/Address.groovy
@@ -1,13 +1,22 @@
 package com.makingdevs.demo.model
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 public class Address{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id
 
 	private String street
 	private Integer outdoorNumber
 	private Integer interiorNumber
 	private Integer postCode
 	private String neighborhood
-	private String state 
+	private String state
 	private String city
 	private String country
 }

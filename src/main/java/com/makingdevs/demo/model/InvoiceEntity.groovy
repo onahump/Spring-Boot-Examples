28441ef528eada13c3253d8ed373a66426994474
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 13:22:36 -0600

    Creating the Entity into InvoiceEntity class & generating its id

--- src/main/java/com/makingdevs/demo/model/InvoiceEntity.groovy
+++ src/main/java/com/makingdevs/demo/model/InvoiceEntity.groovy
@@ -1,13 +1,19 @@
 package com.makingdevs.demo.model
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+@Entity
 public class InvoiceEntity {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id
+
 	private String businessName
 	private String rfc
 	private List<Concept> concepts
-
-	public String toString(){
-		"$businessName / $rfc" 
-	}
-
 }

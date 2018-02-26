db7acf00b235ba112b77aca45bcfdf22d6e933d8
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 13:20:28 -0600

    Conerting Invoice to Entity & generating its id

--- src/main/java/com/makingdevs/demo/model/Invoice.groovy
+++ src/main/java/com/makingdevs/demo/model/Invoice.groovy
@@ -1,8 +1,17 @@
 package com.makingdevs.demo.model
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 public class Invoice {
 
-	private UUID id
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private id
+
 	private Date date = new Date()
 	private InvoiceEntity emitter
 	private InvoiceEntity receiver

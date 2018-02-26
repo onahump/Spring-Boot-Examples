829d19d76618f059c934602d0eee5f9d656dd7c9
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 13:21:53 -0600

    Creating the Entity into Concept class & generating its id

--- src/main/java/com/makingdevs/demo/model/Concept.groovy
+++ src/main/java/com/makingdevs/demo/model/Concept.groovy
@@ -1,6 +1,16 @@
 package com.makingdevs.demo.model
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 public class Concept {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id
+
 	private Integer quantity
 	private String product
 	private BigDecimal price

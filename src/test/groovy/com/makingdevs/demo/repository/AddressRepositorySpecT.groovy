f654ae5722dd577f769fbf93a20f0be41bf44d0b
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-27 22:45:38 -0600

    Testing Address creation in DB

--- src/test/groovy/com/makingdevs/demo/repository/AddressRepositorySpecT.groovy
+++ src/test/groovy/com/makingdevs/demo/repository/AddressRepositorySpecT.groovy
@@ -2,36 +2,42 @@ package com.makingdevs.demo.repository
 
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 import com.makingdevs.demo.model.Address
 import com.makingdevs.demo.repository.AddressRepository
 
-
+@DataJpaTest
 @SpringBootTest
 class AddressTest extends Specification{
 
     @Autowired
     AddressRepository addressRepository
 
+    Address addressX = new Address(street:"Boulevard Tomorrow",
+                                  outdoorNumber: 4,
+                                  interiorNumber: 7,
+                                  postCode: 6678,
+                                  neighborhood: "London",
+                                  state: "Mexico",
+                                  city: "Ecatepec",
+                                  country: "México")
+
     @Test
-    def 'createAddress'(){
-        when:
+    def 'find Address by Id'(){
 
-        address = new Address(street:"Boulevard Tomorrow",
-                                    outdoorNumber: 4,
-                                    interiorNumber: 7,
-                                    postCode: 6678,
-                                    neighborhood: "London",
-                                    state: "Mexico",
-                                    city: "Ecatepec",
-                                    country: "México"
-        )
+        def savedAddressEntity = addressRepository.save(addressX)
 
-        then:
-        
+        when: "load Address Entity"
+
+        def AddressFromDb = addressRepository.findOne(savedAddressEntity.id)
+
+        then: "saved and retrieved entity by id must be equal"
+
+        savedAddressEntity.getId() == AddressFromDb.getId()
+
     }
-
 
 }
